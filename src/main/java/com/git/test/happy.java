ff1411fe4ad3df3a4e92d5c6f17c8a639b7a277b
eclipse_user <eclipse_user@TuHaoj>
2019-10-24 15:26:55 +0800

    left conflict

--- src/main/java/com/git/test/happy.java
+++ src/main/java/com/git/test/happy.java
@@ -3,7 +3,6 @@ package com.git.test;
 public class happy {
 
 	public static void main(String[] args) {
-		System.out.println("Test....");
-		System.out.println("new Test....");
+		System.out.println("left....");
 	}
 }
